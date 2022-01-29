912fbd233677cfeb4f3c8e7649028695aada30e0
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-01-30 00:33:51 +0900

    ESLint 対応

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,20 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    // "@vue/eslint-config-prettier", // plugin も含まれているが不要なので無効化
+    "eslint-config-prettier", // ↑ の代わりにこれを使う
   ],
   env: {
     "vue/setup-compiler-macros": true,
   },
+  overrides: [
+    {
+      // eslint:recommended のルールを無効化
+      // @typescript-eslint/eslint-recommended だと .ts, .tsx のみなので .vue にも適用
+      files: ["*.vue"],
+      rules:
+        require("@typescript-eslint/eslint-plugin/dist/configs/eslint-recommended")
+          .overrides[0].rules,
+    },
+  ],
 };
